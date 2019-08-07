2777b4d26fd085712740f1660a3d339f4c066a3f
Oscar Velandia <odvs12@gmail.com>
2019-08-07 15:19:39 -0300

    Modificaciones
    Modifico los nombres de los arrays filtrados, quito los filtros y copias de arrays innecesarias, por último, modifiqué la función doSum para que use un reduce y la función listWithThirdFilterApplied para quitar el pivotNumber con destructuring.

--- 2Kata.js
+++ 2Kata.js
@@ -1,6 +1,4 @@
-// 1er ejercicio de http://www.medallia.com.ar/internships/doc/2018.pdf
-
-const upperLimit = 21000;
+const upperLimit = 23000;
 // const numbers = [
 //   1, 10, 100, 12, 15, 16, 17, 18, 19, 21, 22, 23, 24,
 //   26, 27, 28, 29, 3, 35, 37, 39, 4, 40, 43, 45, 46, 47,
@@ -8,41 +6,37 @@ const upperLimit = 21000;
 //   68, 7, 71, 72, 73, 75, 77, 79, 8, 80, 81, 82, 83, 84,
 //   86, 87, 88, 89, 9, 90, 92, 93, 94, 97, 98, 99
 // ]
-
-const numbers = new Array(21000).fill().map((_, index) => index + 1);
+const numbers = new Array(23000).fill().map((_, index) => index + 1);
 const numbersLength = numbers.length;
 
 
 function orchestrateDataClean(upperLimit, listLength, numbers) {
-  const firstNumberFilter = numbers.filter(num => num < upperLimit);
+  const listWithFirstFilterApplied = numbers.filter(num => num < upperLimit);
 
-  const secondNumberFilter = [...firstNumberFilter]
+  const listWithSecondFilterApplied = listWithFirstFilterApplied
     .map((_, index) => {
-      const copyFirstFilteredNumbers = [...firstNumberFilter];
+      // Se copia para poder hacer el splice en el array pasado al tercer filtro. 
+      const copyToModify = [...listWithFirstFilterApplied];
 
-      if (copyFirstFilteredNumbers.length > (index + 1)) {
-        return thirdNumberFilter(copyFirstFilteredNumbers.splice(index), upperLimit);
+      if (listWithFirstFilterApplied.length > (index + 1)) {
+        return listWithThirdFilterApplied(copyToModify.splice(index), upperLimit);
       } 
     })
     .filter(array => array && array.length); // Para quitar arrays vacios
   
-  return doSum(secondNumberFilter);
+  return doSum(listWithSecondFilterApplied);
 }
 
-function thirdNumberFilter(array, upperLimit) {
-  const pivotNumber = array[0];
+function listWithThirdFilterApplied(array, upperLimit) {
+  const [pivotNumber, ...arrayWithoutPivot] = array;
   const upperLimitInFilterArray = upperLimit - pivotNumber;  
   
-  return array
-    .filter(num => num !== pivotNumber)
-    .filter(num => num < upperLimitInFilterArray );
+  return arrayWithoutPivot
+    .filter(num => num < upperLimitInFilterArray);
 }
 
 function doSum(array) {
-  let arraySum = 0;
-  array.forEach(arr => arraySum += arr.length);
-
-  return arraySum;
+  return array.reduce((acc, subArray) => acc + subArray.length, 0);
 }
 
 console.log(orchestrateDataClean(upperLimit, numbersLength, numbers));
