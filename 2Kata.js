4699bc839c9c2de4344daf462a7b1f4c5ded5401
Oscar Velandia <odvs12@gmail.com>
2019-08-07 00:10:19 -0300

    Terminación del ejercicio
    Falta arreglar error con el 15

--- 2Kata.js
+++ 2Kata.js
@@ -1,32 +1,35 @@
-const maxNumber = 10;
-const numbersLength = 4;
-const numbers = [3, 2, 8, 7, 4, 9, 5]
+const maxNumber = 20;
+const numbersLength = 12;
+const numbers = [3, 2, 8, 7, 4, 9, 5, 15, 6, 1, 10, 17]
+// TODO arreglar bug con el 15
+function cleanGeneralData(upperLimit, listLength, numbers) {
+  const firstFilteredNumbers = numbers.filter(num => num < upperLimit);
+  const arrayToSum = firstFilteredNumbers
+    .map((_, index) => {
+      const copyFirstFilteredNumbers = [...firstFilteredNumbers];
 
-function cleanGeneralData(maxNumber, listLength, numbers) {
-  const numbersToIterate = numbers.filter(num => num < maxNumber);
-
-  const arrayToDetectSum = numbersToIterate
-    .map(num => cleanDataWithEveryNumber(num, maxNumber, numbersToIterate))
-    .map((array, i) => array.splice(i))
-    .filter(array => array.length > 1);
-
-  console.log(arrayToDetectSum)
-  // arrayToDetectSum.map(singleArray => sumEveryArrayOfNumbers(singleArray, maxNumber));
-}
-
-function cleanDataWithEveryNumber(number, upperLimit, allNumbers) {
-  const numberToDelete = upperLimit - number;
-  const filteredNumbersArray = allNumbers.filter(num => num < numberToDelete);
-  console.log(filteredNumbersArray)
-  return filteredNumbersArray;
-}
-
-function sumEveryArrayOfNumbers(numbersToSum, maxNumber) {
-  let pivotNumber = numbersToSum.shift();
+      if (firstFilteredNumbers.length > (index + 1)) {
+        return secondDataFilter(copyFirstFilteredNumbers.splice(index), upperLimit);
+      } 
+    })
+    .filter(array => array && array.length)
   
-  numbersToSum.map(num => maxNumber - num);
-  
-  // console.log(numberOfPairs);
+    console.log(arrayToSum)
+  return doSum(arrayToSum);
 }
 
-cleanGeneralData(maxNumber, numbersLength, numbers);
+function secondDataFilter(array, upperLimit) {
+  const pivotNumber = array[0];
+  const upperLimitInFilterArray = upperLimit - pivotNumber;  
+  console.log(upperLimitInFilterArray)
+  return array.filter(num => num < upperLimitInFilterArray);
+}
+
+function doSum(array) {
+  const arrayLength = array.length;
+  const allArrayItemsQuantity = [].concat(...array).length;
+
+  return allArrayItemsQuantity - arrayLength;
+}
+
+console.log(cleanGeneralData(maxNumber, numbersLength, numbers));
